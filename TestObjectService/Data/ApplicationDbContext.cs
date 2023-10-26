d91118180b5cf26acda839abbfe742177701f699
davidmsf <davidmsf@gmail.com>
2023-10-26 10:36:18 +0200

    created Model and context

---
+++ TestObjectService/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestObjectService.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestObjectService.Models.TestObject> TestObjects { get; set; }
+    }
+}
